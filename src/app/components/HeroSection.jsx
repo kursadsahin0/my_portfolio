3aa55f2bde1b5a2ab7ea3406514db3563f579df5
Kürşat <90332216+kursadsahin0@users.noreply.github.com>
2023-10-27 16:29:15 +0300

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
                     </h1>
                     <p className='text-[#ADB7BE] sm:text-lg lg:text-xl mb-6 text-base '>I am interested in Web development since second year of the university. I started to develop projects with Javascript and React. I developed myself in terms of research, self-motivation and acquiring different abilities. I am a team player, willing to be trained for growth. I am believe that everyday is a new start.</p>
                     <div>
-                        <Link href='kursatsahin565@gmail.com' className='px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white w-full sm:w-fit'>Hire Me</Link>
+                        <Link href='https://www.linkedin.com/in/kursat-sahin0/' className='px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white w-full sm:w-fit'>Hire Me</Link>
                         <button className='px-1 py-1 rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3 w-full sm:w-fit'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
                     </div>
                 </div>
