89b8eeca08ead0773ad3aa5be340bd4ef3f66983
Kürşat <90332216+kursadsahin0@users.noreply.github.com>
2023-10-27 15:20:40 +0300

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,13 +36,13 @@ const HeroSection = () => {
                     </h1>
                     <p className='text-[#ADB7BE] sm:text-lg lg:text-xl mb-6 text-base '>I'm interested in Web development since second year of the university. I started to develop projects with Javascript and React. I developed myself in terms of research, self-motivation and acquiring different abilities. I'm a team player, willing to be trained for growth. I'm believe that everyday is a new start.</p>
                     <div>
-                        <Link href="/contact" className='px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white w-full sm:w-fit'>Hire Me</Link>
+                        <Link href='/contact' className='px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white w-full sm:w-fit'>Hire Me</Link>
                         <button className='px-1 py-1 rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3 w-full sm:w-fit'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
                     <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-                        <Image src="/images/heroProfile.png" alt='hero Image' className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' width={300} height={300} />
+                        <Image src='/images/heroProfile.png' alt='hero Image' className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' width={300} height={300} />
                     </div>
                 </div>
             </div>
